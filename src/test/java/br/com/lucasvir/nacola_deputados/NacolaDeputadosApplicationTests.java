fc06b9aabfcc1f6e321ad7401cb877d9098b67a1
lucasvir <lavirmond@gmail.com>
2024-06-06 14:27:46 -0300

    initial :)

---
+++ src/test/java/br/com/lucasvir/nacola_deputados/NacolaDeputadosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lucasvir.nacola_deputados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NacolaDeputadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
